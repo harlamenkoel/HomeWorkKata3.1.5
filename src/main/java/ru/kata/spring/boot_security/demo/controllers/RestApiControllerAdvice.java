90088158afe7b316d14d728eb2ff37595a3eb9a9
harlamenkoel <harlamenkoel@gmail.com>
2023-06-01 18:50:46 +0700

    Исправил

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RestApiControllerAdvice.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RestApiControllerAdvice.java
@@ -2,34 +2,24 @@ package ru.kata.spring.boot_security.demo.controllers;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import ru.kata.spring.boot_security.demo.util.UserErrorResponse;
-import ru.kata.spring.boot_security.demo.util.UserNotFoundException;
 
 import java.util.List;
+
 
 @ControllerAdvice
 public class RestApiControllerAdvice {
 
-    @ExceptionHandler(UserNotFoundException.class)
-    public ResponseEntity<UserErrorResponse> handleUserNotFoundException(UserNotFoundException e) {
-        UserErrorResponse response = new UserErrorResponse(
-                e.getMessage(), System.currentTimeMillis()
-        );
-        return new ResponseEntity<>(response, HttpStatus.NOT_FOUND);
-    }
-
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<UserErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        BindingResult bindingResult = e.getBindingResult();
-        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        List<ObjectError> errors = e.getAllErrors();
         StringBuilder errorMsg = new StringBuilder();
-        for (FieldError fieldError : fieldErrors) {
-            errorMsg.append(fieldError.getField()).append(" - ").append(fieldError.getDefaultMessage()).append(";");
+        for (ObjectError error : errors) {
+            errorMsg.append(error.getDefaultMessage()).append(";");
         }
         UserErrorResponse response = new UserErrorResponse(
                 errorMsg.toString(), System.currentTimeMillis()
