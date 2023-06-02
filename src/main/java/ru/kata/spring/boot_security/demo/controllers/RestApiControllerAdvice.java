11b1987654f44b3a52550ba7777c06d7199fb35b
harlamenkoel <harlamenkoel@gmail.com>
2023-06-02 16:43:01 +0700

    Исправил

--- src/main/java/ru/kata/spring/boot_security/demo/controllers/RestApiControllerAdvice.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/RestApiControllerAdvice.java
@@ -1,29 +1,25 @@
 package ru.kata.spring.boot_security.demo.controllers;
 
-import org.springframework.http.HttpStatus;
+
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import ru.kata.spring.boot_security.demo.util.UserErrorResponse;
 
-import java.util.List;
+import java.util.stream.Collectors;
 
 
 @ControllerAdvice
 public class RestApiControllerAdvice {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<UserErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-        List<ObjectError> errors = e.getAllErrors();
-        StringBuilder errorMsg = new StringBuilder();
-        for (ObjectError error : errors) {
-            errorMsg.append(error.getDefaultMessage()).append(";");
-        }
-        UserErrorResponse response = new UserErrorResponse(
-                errorMsg.toString(), System.currentTimeMillis()
-        );
-        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String errorMessage = e.getAllErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.joining(";"));
+
+        return ResponseEntity.badRequest().body(errorMessage);
     }
 }
